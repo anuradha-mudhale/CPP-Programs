8d90a8a480f454d40c65759c4832bc572e2e209b
anuradha-mudhale <anuradhamudhale314@gmail.com>
2025-04-25 23:54:17 +0530

    Add files via upload

---
+++ areaOfSquare.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int side;
+	float areaOfSquare;
+	cout<<"Enter the side value";
+	cin>>side;
+	areaOfSquare = side*side;
+	cout<<"Area of Square:"<<areaOfSquare;
+	return 0;
+}
